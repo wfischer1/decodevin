2526aa90c9c99b89fbea37696f9c3d30c248de7c
Wolfgang Fischer <wolfgang.fischer@senacor.com>
2017-06-23 14:16:15 +0200

    decodevon fixed

--- src/main/java/com/senacor/academy/decodevin/DecodeVin.java
+++ src/main/java/com/senacor/academy/decodevin/DecodeVin.java
@@ -4,37 +4,34 @@ import com.senacor.academy.decodevin.model.VehicleData;
 
 public class DecodeVin {
 
-  private static final String YEARCHARS = "ABCDEFGHJKLMNPRSTVWXY123456789";
+    private static final String YEARCHARS = "ABCDEFGHJKLMNPRSTVWXY123456789";
 
-  public VehicleData decodeVin(String vin) {
-    VehicleData data = new VehicleData();
+    public VehicleData decodeVin(String vin) {
+        VehicleData data = new VehicleData();
 
-    data.setWmi("unknown");
-    data.setVds("unknown");
-    data.setVis("unknown");
-    data.setYear(0);
+        data.setWmi("unknown");
+        data.setVds("unknown");
+        data.setVis("unknown");
+        data.setYear(0);
 
-    /* reference solution is here:
-     
-    // wmi is chars 1..3
-    data.setWmi(vin.substring(0, 3));
+        // wmi is chars 1..3
+        data.setWmi(vin.substring(0, 3));
 
-    // vds is chars 4..9
-    data.setVds(vin.substring(3, 9));
+        // vds is chars 4..9
+        data.setVds(vin.substring(3, 9));
 
-    // vis is chars 10..17
-    data.setVis(vin.substring(9));
+        // vis is chars 10..17
+        data.setVis(vin.substring(9));
 
-    char yearCode = vin.charAt(9);
+        char yearCode = vin.charAt(9);
 
-    int year = YEARCHARS.indexOf(yearCode);
-    if (year > 0) {
-      data.setYear(1980 + year);
-    } else {
-      data.setYear(0);
+        int year = YEARCHARS.indexOf(yearCode);
+        if (year > 0) {
+            data.setYear(1980 + year);
+        } else {
+            data.setYear(0);
+        }
+
+        return data;
     }
-    */
-
-    return data;
-  }
 }
